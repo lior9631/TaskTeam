44bc2e3002ea32de56d918718614a4e6536595a3
Alon Davidi <alon359@gmail.com>
2019-11-08 16:59:47 +0200

    update

--- project/TeamTask/src/app/app.module.ts
+++ project/TeamTask/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { OpenPageComponent } from './open-page/open-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OpenPageComponent
   ],
   imports: [
     BrowserModule
