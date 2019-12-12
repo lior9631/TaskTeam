303d2aa6eb14c9f830d4b0e930ac37a38f7cee1c
Alon Davidi <alon359@gmail.com>
2019-12-12 12:37:25 +0200

    update

--- project/p1/taskTeam/src/app/app.module.ts
+++ project/p1/taskTeam/src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './homePage/home-page/home-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent
-  ],
+   ],
   imports: [
     BrowserModule
   ],
