56a6c7b9e2ae712c508c89fd87204fc06d202570
lior9631 <55757487+lior9631@users.noreply.github.com>
2019-12-30 23:26:53 +0200

    update

--- project/p1/taskTeam/src/app/app.module.ts
+++ project/p1/taskTeam/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Component } from '@angular/core';
 import {RouterModule, Routes } from '@angular/router';
+import { Route } from '@angular/compiler/src/core';
+import { from } from 'rxjs';
+
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { NavBarComponent } from './home-page/nav-bar/nav-bar.component';
-import { Route } from '@angular/compiler/src/core';
-import { from } from 'rxjs';
 import { LogInComponent } from './home-page/log-in/log-in.component';
 import { SignUpComponent } from './home-page/sign-up/sign-up.component';
 
